f05fcad8319a881eddee16fe0a9de0b2deb16447
Jason Croft <croftbc@gmail.com>
2016-04-26 21:53:38 -0500

    Update veriflow to copy topology file (if it exists) to class dir

--- Parsers/VeriFlow-v0.2/VeriFlow/Test.cpp
+++ Parsers/VeriFlow-v0.2/VeriFlow/Test.cpp
@@ -33,145 +33,131 @@
 #include "../veriflow-util/openflow.h"
 #include "../veriflow-util/StringTokenizer.h"
 #include <sstream>
-
 using namespace std;
-
 
 extern VeriFlow veriflow;
 void testRocketfuelRouteViewsDataset()
 {
-	string baseDir = "data";
-	DIR *dp;
-	struct dirent *dirp;
-	vector< string > deviceFileList;
+    string baseDir = "data";
+    DIR *dp;
+    struct dirent *dirp;
+    vector< string > deviceFileList;
 
-	if((dp = opendir(baseDir.c_str())) == NULL)
+    if((dp = opendir(baseDir.c_str())) == NULL)
+    {
+	fprintf(stderr, "Error opening %s\n", baseDir.c_str());
+	return;
+    }
+
+    while((dirp = readdir(dp)))
+    {
+	if((strcmp(dirp->d_name, ".") == 0) || (strcmp(dirp->d_name, "..") == 0)||dirp->d_name[0]!='R')
 	{
-		fprintf(stderr, "Error opening %s\n", baseDir.c_str());
-		return;
+	    continue;
 	}
 
-	while((dirp = readdir(dp)))
+	string fileName = baseDir + "/" + dirp->d_name;
+	deviceFileList.push_back(fileName);
+
+    }
+
+    closedir(dp);
+
+//fprintf(stdout, "Total device count: %lu\n", deviceFileList.size());
+//fflush(stdout);
+
+    char buffer[1024];
+    for(unsigned int i = 0; i < deviceFileList.size(); i++)
+    {
+	fprintf(stdout, "[%u] Processing %s\n", i, deviceFileList[i].c_str());
+	fflush(stdout);
+	ifstream fin(deviceFileList[i].c_str());
+	while(fin.eof() == false)
 	{
-		if((strcmp(dirp->d_name, ".") == 0) || (strcmp(dirp->d_name, "..") == 0)||dirp->d_name[0]!='R')
-		{
-			continue;
-		}
+	    fin.getline(buffer, 1023);
+	    if(strlen(buffer) == 0)
+	    {
+		continue;
+	    }
 
-		string fileName = baseDir + "/" + dirp->d_name;
-		deviceFileList.push_back(fileName);
+	    StringTokenizer st(buffer, " ");
+	    Rule rule;
+	    rule.type = FORWARDING;
+	    rule.fieldValue[DL_SRC] = "0:0:0:0:0:0";
+	    rule.fieldMask[DL_SRC] = "0:0:0:0:0:0";
+	    rule.fieldValue[DL_DST] = "0:0:0:0:0:0";
+	    rule.fieldMask[DL_DST] = "0:0:0:0:0:0";
+	    rule.fieldValue[NW_SRC] = "0.0.0.0";
+	    rule.fieldMask[NW_SRC] = "0.0.0.0";
+	    st.nextToken();
+	    st.nextToken();
+	    rule.fieldValue[NW_DST] = st.nextToken();
+	    if(rule.fieldValue[NW_DST].find('/') != string::npos)
+	    {
+		rule.fieldValue[NW_DST] = rule.fieldValue[NW_DST].substr(0, rule.fieldValue[NW_DST].find('/'));
+	    }
 
+	    rule.fieldMask[NW_DST] = st.nextToken();
+	    rule.wildcards = OFPFW_ALL;
+	    rule.location = st.nextToken();
+	    rule.nextHop = st.nextToken();
+	    st.nextToken();
+
+	    rule.priority = atoi(st.nextToken().c_str());
+	    veriflow.addRule(rule);
 	}
 
-	closedir(dp);
-
-//	fprintf(stdout, "Total device count: %lu\n", deviceFileList.size());
-//	fflush(stdout);
-
-	char buffer[1024];
-
-
-
-	for(unsigned int i = 0; i < deviceFileList.size(); i++)
-	{
-		fprintf(stdout, "[%u] Processing %s\n", i, deviceFileList[i].c_str());
-		fflush(stdout);
-		ifstream fin(deviceFileList[i].c_str());
-		while(fin.eof() == false)
-		{
-			fin.getline(buffer, 1023);
-			if(strlen(buffer) == 0)
-			{
-				continue;
-			}
-
-
-			StringTokenizer st(buffer, " ");
-			Rule rule;
-
-
-			rule.type = FORWARDING;
-			rule.fieldValue[DL_SRC] = "0:0:0:0:0:0";
-			rule.fieldMask[DL_SRC] = "0:0:0:0:0:0";
-			rule.fieldValue[DL_DST] = "0:0:0:0:0:0";
-			rule.fieldMask[DL_DST] = "0:0:0:0:0:0";
-			rule.fieldValue[NW_SRC] = "0.0.0.0";
-			rule.fieldMask[NW_SRC] = "0.0.0.0";
-			st.nextToken();
-			st.nextToken();
-			rule.fieldValue[NW_DST] = st.nextToken();
-			if(rule.fieldValue[NW_DST].find('/') != string::npos)
-			{
-				rule.fieldValue[NW_DST] = rule.fieldValue[NW_DST].substr(0, rule.fieldValue[NW_DST].find('/'));
-			}
-
-			rule.fieldMask[NW_DST] = st.nextToken();
-			rule.wildcards = OFPFW_ALL;
-			rule.location = st.nextToken();
-			rule.nextHop = st.nextToken();
-			st.nextToken();
-
-			rule.priority = atoi(st.nextToken().c_str());
-
-
-			veriflow.addRule(rule);
-		}
-
-		fin.close();
-	}
+	fin.close();
+    }
 }
-
-
-
-
-
 
 void Test::test()
 {
-  //Test::testVerification();
-	FILE* fout = fopen("time.out", "w");
-	struct timeval t, t1;
-	gettimeofday(&t, NULL);
-	testRocketfuelRouteViewsDataset();
-	vector< EquivalenceClass > vFinalPacketClasses;
-	vector< vector< Trie* > > vFinalTries;
-	
-	veriflow.getAllEquivalenceClasses(vFinalPacketClasses, vFinalTries);
+    //Test::testVerification();
+    FILE* fout = fopen("time.out", "w");
+    struct timeval t, t1;
+    gettimeofday(&t, NULL);
+    testRocketfuelRouteViewsDataset();
+    vector< EquivalenceClass > vFinalPacketClasses;
+    vector< vector< Trie* > > vFinalTries;
 
-	cout<<"Number of classes: "<<vFinalPacketClasses.size()<<endl;
+    veriflow.getAllEquivalenceClasses(vFinalPacketClasses, vFinalTries);
 
-	for(unsigned  i =0;  i< vFinalPacketClasses.size(); i++ )
+    cout<<"Number of classes: "<<vFinalPacketClasses.size()<<endl;
+
+    for(unsigned  i =0;  i< vFinalPacketClasses.size(); i++ )
+    {
+	//cout<<itr->toString();
+	//cout<<endl;
+	ForwardingGraph *g=veriflow.getForwardingGraph(vFinalTries[i], vFinalPacketClasses[i]);
+	cout<<"Writing Graph "<<i<<endl;
+	stringstream filename;
+	filename<<"class/"<<i<<".txt";
+	ofstream myfile(filename.str().c_str(),std::ofstream::out);
+
+	for(unordered_map<string,list<ForwardingLink> >::const_iterator itr2=g->links.begin();itr2!=g->links.end();itr2++)
 	{
-		//cout<<itr->toString();		
-		//cout<<endl;
-	 	ForwardingGraph *g=veriflow.getForwardingGraph(vFinalTries[i], vFinalPacketClasses[i]);
-		cout<<"Writing Graph "<<i<<endl;
-		stringstream filename; 
-		filename<<"class/"<<i<<".txt"; 
-		ofstream myfile(filename.str().c_str(),std::ofstream::out); 
-
-	  	for(unordered_map<string,list<ForwardingLink> >::const_iterator itr2=g->links.begin();itr2!=g->links.end();itr2++)
-		{
-		 	  myfile<<i<<" "<<itr2->first<<" "<<itr2->second.front().rule.nextHop<<endl;
-			   
-		}
-
-		myfile.close(); 
+	    myfile<<i<<" "<<itr2->first<<" "<<itr2->second.front().rule.nextHop<<endl;
 
 	}
-	cout<<"done"; 
 
-	//	ForwardingGraph *g=veriflow.getForwardingGraph(vFinalTries[1],vFinalPacketClasses[1]);
-	//	cout<<g->toString();
+	myfile.close();
+    }
 
-  
+    //	ForwardingGraph *g=veriflow.getForwardingGraph(vFinalTries[1],vFinalPacketClasses[1]);
+    //	cout<<g->toString();
 
+    if (fopen("data/topo.txt", "r")) {
+	cout << "\ncopying topology\n";
+	std::ifstream src("data/topo.txt", std::ios::binary);
+	std::ofstream dst("class/topo.txt", std::ios::binary);
 
+	dst << src.rdbuf();
+    }
 
+    cout<<"done";
 
-  
-	gettimeofday(&t1, NULL);
-	fprintf(fout, "%f\n", t1.tv_sec - t.tv_sec + 1E-6 * (t1.tv_usec - t.tv_usec));
-	fclose(fout);
-
+    gettimeofday(&t1, NULL);
+    fprintf(fout, "%f\n", t1.tv_sec - t.tv_sec + 1E-6 * (t1.tv_usec - t.tv_usec));
+    fclose(fout);
 }
