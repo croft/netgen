02f1e8d23e95c9e0aec4807041d7d50b3336d95e
Shambwaditya Saha <ssaha6@illinois.edu>
2016-06-05 13:17:52 -0500

    delete rocket fuel

--- data_set/RocketFuel/grep.sh
+++
@@ -1,5 +0,0 @@
-for f in *
-do
-  grep  "10.0.3.102"  $f #| xargs grep -l "10.0.1.153" | xargs grep -l "10.0.2.154"
-
-done
--- data_set/RocketFuel/select.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-filename="select.txt"
-while read -r line
-do
-    name=$line
-    cp class/$name selected/$name
-done < "$filename"
