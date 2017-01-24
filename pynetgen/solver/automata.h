6a8688f7f9110abbb25b673b0db4df901962bbd4
Shambwaditya Saha <ssaha6@illinois.edu>
2017-01-24 04:14:54 -0600

    symbol reduced automata

--- pynetgen/solver/automata.h
+++ pynetgen/solver/automata.h
@@ -35,6 +35,14 @@ class Automata {
         set<int> final_states;
         int dead_state;
 
+        map<int,int> reduced_symbols;
+        map<int,int> inverse_reduced_symbols;
+        map<pair<int, int>, int> reduced_transitions;
+        int num_reduced_symbol;
+        void Compute_symbol_reduction();
+        void Compute_transition_reduction(); 
+        
+        
         friend std::ostream &operator<<(std::ostream &stream, const Automata &a1)
         {
                 stream << "\nAutomata \nstates:" << a1.states;
@@ -46,4 +54,89 @@ class Automata {
         }
 };
 
+void Automata::Compute_symbol_reduction()
+{       
+        std::deque<int> workspace(symbols.begin(),symbols.end());
+        int K = -1;
+        
+        while(!workspace.empty())
+        {       
+                int element = workspace.front();
+                workspace.pop_front();
+                K = K + 1;
+                reduced_symbols[element] = K;
+                inverse_reduced_symbols[K] = element;
+                
+                //for all other elements in queue
+                for (auto symbol_it = workspace.begin(); symbol_it != workspace.end(); ) 
+                {       
+                        //if element matches with some other symbol for all states
+                        int flag = true;
+                        for(auto st: states)
+                        {
+                                if(transitions[make_pair(st,*symbol_it)] != transitions[make_pair(st,element)])
+                                {
+                                        flag = false;
+                                        break;
+                                }
+                        }        
+                        
+                        //if so delete the symbol from workspace, and assign it the index of element
+                        if(flag == true)
+                        {
+                                 reduced_symbols[*symbol_it] = K;
+                                 symbol_it = workspace.erase(symbol_it);
+                        }
+                        //else move on to the next symbol 
+                        else
+                        {
+                                symbol_it++;
+                        }
+                }     
+        }
+        num_reduced_symbol = K;
+}
+
+void Automata::Compute_transition_reduction()
+{
+        Compute_symbol_reduction();
+                        
+        for( auto st : states)
+        {
+                for( auto mp : inverse_reduced_symbols)
+                {
+                        //reduced_transition[state, reduced_symbol] = transitions[state,symbol]
+                        //correctness assured by the fact that all equivalence symbols, map to same state
+                        reduced_transitions[make_pair(st, mp.first)] = transitions[make_pair(st, mp.second)];
+                }
+        }                
+}
+
+
 #endif
+
+        // void Network::Compute_AlphabetCongruence()
+        // {
+        //       index = 0;
+        //       set<int> result;
+        //         for all alphabet: 10000
+        //                 empty string
+        //                 25 : for all state: 5
+        //                         concat string 5
+        //                 insert(result,string) :log(5)
+        //                 uniq[collection], check and insert (10)
+
+        //                 if(insrted)
+        //                         index++;
+
+        //                         result[alphabet] = k;
+
+        // workspace = all alphabets...10000
+        // while (workspace != empty) 10
+        //         pick one form workspace
+        //         50,000
+        //         for all alphabet
+        //                 for all q
+        //                         check if delta is is_same: concat really not helpful...
+        //         remove all alphabet 10000
+        // }
